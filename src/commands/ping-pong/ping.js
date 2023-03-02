d67ed293dfd4f321cc5550feb3a36f13efd9f9d4
Matzsche <66742471+Mack68426@users.noreply.github.com>
2023-03-02 22:40:20 +0800

    [Discord bot] A bot based on djs

---
+++ src/commands/ping-pong/ping.js
@@ -0,0 +1,19 @@
+import {SlashCommandBuilder} from 'discord.js'
+
+
+// 建立斜線指令
+const command =  new SlashCommandBuilder()
+    .setName("ping")
+    .setDescription("ping command")
+
+// 執行指令後的動作
+const action = async (inters) => { 
+    inters.reply("pong!") 
+}
+
+
+export {
+    command,
+    action
+}
+
